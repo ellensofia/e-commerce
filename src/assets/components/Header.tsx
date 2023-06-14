7310419f734dc4193321b2844a8746fbed302a40
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-06-14 23:10:31 +0200

    add some style to header (WIP)

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,17 +1,53 @@
+import { styled } from "styled-components";
+
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <a>Logotype</a>
       <nav>
-        <ul>
-          <li>
+        <StyledMiddle>
+          <StyledListItem>
             <a href="">Home</a>
+          </StyledListItem>
+          <StyledListItem>
             <a href="">About</a>
+          </StyledListItem>
+          <StyledListItem>
             <a href="">Contact</a>
+          </StyledListItem>
+          <StyledListItem>
             <a href="">Find us</a>
-          </li>
-        </ul>
+          </StyledListItem>
+        </StyledMiddle>
       </nav>
-    </header>
+      <StyledRight>
+        <div>searchbar</div>
+        <div>menu</div>
+      </StyledRight>
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  display: fixed;
+  display: flex;
+  justify-content: space-between;
+  top: 0;
+  border-bottom: 1px solid black;
+  align-items: center;
+  padding: 0 2rem;
+`;
+
+const StyledListItem = styled.li`
+  list-style: none;
+`;
+
+const StyledMiddle = styled.ul`
+  display: flex;
+  gap: 1rem;
+`;
+
+const StyledRight = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
