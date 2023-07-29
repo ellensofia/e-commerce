5c764f9154f136986d67ee5b5dd045993568e91b
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-07-29 21:43:33 +0200

    carousel changes opacity instead of translateX, no longer adds images when new images is shown

--- src/assets/components/Carousel.tsx
+++ src/assets/components/Carousel.tsx
@@ -6,68 +6,80 @@ import image3 from "../images/image3.jpg";
 
 const images = [image1, image2, image3];
 
+interface StyledButtonProps {
+  isActive: boolean;
+}
+
+interface StyledItemProps {
+  zIndex: number;
+  isActive: boolean;
+}
+
 export default function Carousel() {
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
   const [delay, setDelay] = useState(8000);
-  const [carouselImages, setCarouselImages] = useState(images);
+  let interval: ReturnType<typeof setInterval>;
 
   const navigate = (direction: string) => {
+    // Stop the interval
+    clearInterval(interval);
+
     if (direction === "left") {
-      if (currentItemIndex === 0) {
-        const newImageIndex = currentItemIndex % images.length;
-        const newImage = images[newImageIndex];
-        setCarouselImages((prevImages) => [...prevImages, newImage]);
-        setCurrentItemIndex(currentItemIndex - 1);
-      } else {
-        setCurrentItemIndex(currentItemIndex - 1);
-      }
+      setCurrentItemIndex(0);
+    } else if (direction === "middle") {
+      setCurrentItemIndex(1);
     } else if (direction === "right") {
-      const newImageIndex = currentItemIndex % images.length;
-      const newImage = images[newImageIndex];
-      setCarouselImages((prevImages) => [...prevImages, newImage]);
-      setCurrentItemIndex(currentItemIndex + 1);
+      setCurrentItemIndex(2);
     }
+    // Restart the interval after a short delay (e.g., 2 seconds)
+    setTimeout(() => {
+      interval = setInterval(() => {
+        setCurrentItemIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, delay);
+    }, 2000);
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const newImageIndex = currentItemIndex % images.length;
-      const newImage = images[newImageIndex];
-      setCarouselImages((prevImages) => [...prevImages, newImage]);
-      setCurrentItemIndex(currentItemIndex + 1);
+      setCurrentItemIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, delay);
 
     return () => {
       clearInterval(interval);
     };
-  }, [currentItemIndex, delay, carouselImages]);
+  }, [delay]);
 
   return (
     <>
       <OuterBox>
         <InnerContainer>
-          {carouselImages.map((image, index) => (
-            <StyledCarouselItem key={index}>
-              <div
-                key={index}
-                style={{
-                  transform: `translateX(${(index - currentItemIndex) * 100}%)`,
-                  transition: "transform 1.6s ease-in-out",
-                }}
-              >
-                <StyledImage src={image} alt={image} key={index} />
-                <TextBox>
-                  <h3>Special offer</h3>
-                  <p>50% off</p>
-                </TextBox>
-              </div>
+          {images.map((image, index) => (
+            <StyledCarouselItem
+              key={index}
+              zIndex={currentItemIndex === index ? 1 : 0}
+              isActive={currentItemIndex === index}
+            >
+              <StyledImage src={image} alt={image} key={index} />
+              <TextBox>
+                <h3>Special offer</h3>
+                <p>50% off</p>
+              </TextBox>
             </StyledCarouselItem>
           ))}
         </InnerContainer>
         <Navigations>
-          <StyledButton onClick={() => navigate("left")}></StyledButton>
-          <StyledButton onClick={() => navigate("")}></StyledButton>
-          <StyledButton onClick={() => navigate("right")}></StyledButton>
+          <StyledButton
+            isActive={currentItemIndex === 0}
+            onClick={() => navigate("left")}
+          ></StyledButton>
+          <StyledButton
+            isActive={currentItemIndex === 1}
+            onClick={() => navigate("middle")}
+          ></StyledButton>
+          <StyledButton
+            isActive={currentItemIndex === 2}
+            onClick={() => navigate("right")}
+          ></StyledButton>
         </Navigations>
       </OuterBox>
     </>
@@ -88,8 +100,8 @@ const TextBox = styled.div`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  font-size: 2.6rem;
-  color: #fff;
+  font-size: var(--font-size-xl);
+  color: var(--clr-white);
   font-weight: 400;
   width: 100vw;
   display: flex;
@@ -97,7 +109,6 @@ const TextBox = styled.div`
   align-items: center;
   @media (max-width: 600px) {
     width: 90vw;
-    font-size: 2rem;
   }
 `;
 
@@ -106,11 +117,14 @@ const InnerContainer = styled.div`
   width: 100%;
 `;
 
-const StyledCarouselItem = styled.div`
+const StyledCarouselItem = styled.div<StyledItemProps>`
   position: absolute;
   height: 100%;
   width: 100%;
   overflow: hidden;
+  opacity: ${({ isActive }) => (isActive ? "1" : "0")};
+  transition: opacity 0.5s ease-in-out;
+  z-index: ${({ zIndex }) => zIndex};
 `;
 
 const StyledImage = styled.img`
@@ -142,18 +156,21 @@ const Navigations = styled.div`
   justify-content: center;
   align-items: flex-end;
   gap: 1rem;
-  background-color: #e9cccc6b;
   padding: 1rem 0;
 `;
 
-const StyledButton = styled.div`
-  width: 1.2rem;
-  height: 1.2rem;
+const StyledButton = styled.button<StyledButtonProps>`
+  width: 2.4rem;
+  height: 0.8rem;
+  padding: 0;
   background-color: #ffffffa4;
-  border-radius: 50%;
+  background-color: ${({ isActive }) => (isActive ? "#2a2a2a" : "#ffffffa4")};
+  border-radius: 0;
+  border: 1px solid #2a2a2a;
   cursor: pointer;
+  z-index: 10;
 
   &:hover {
-    background-color: #ffffffbd;
+    background-color: ${({ isActive }) => (isActive ? "#2a2a2a" : "#ffffffbd")};
   }
 `;
