e2cfe4ad12e200a86635fe1bcacaacaab2b85003
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-07-31 21:31:01 +0200

    changed link on product page + style (WIP)

--- src/assets/components/Product.tsx
+++ src/assets/components/Product.tsx
@@ -1,15 +1,13 @@
-import { Rating } from "@mui/material";
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Product } from "../../data";
 export interface Props {
   product: Product;
-  index: number;
 }
 
-export default function Product({ product, index }: Props) {
-  const { image, product_name, prices, reviews, amount } = product;
+export default function Product({ product }: Props) {
+  const { image, id, product_name, prices, reviews, amount } = product;
 
   const [showFullDescription, setShowFullDescription] = useState(false);
   const [selectedAmountIndex, setSelectedAmountIndex] = useState(0);
@@ -21,38 +19,40 @@ export default function Product({ product, index }: Props) {
   const [price, setPrice] = useState(prices[selectedAmountIndex]);
 
   return (
-    <OuterBox>
-      <StyledImage src={image} alt={product_name} key={index} />
-      <ProductText>
-        <Title>{product_name}</Title>
-        <Review>
-          <span>Reviews </span>
+    <>
+      <StyledLink to={`/shop/${product.id}`}>
+        <OuterBox>
+          <StyledImage src={image} alt={product_name} key={id} />
+          <ProductText>
+            <Title>{product_name}</Title>
+            {/* <Review> */}
+            {/* <span>Reviews </span>
           <TotalReviews>({reviews.total_reviews})</TotalReviews>
           <span>{reviews.average_rating}</span>
-        </Review>
-        <Rating
-          name={`rating-${index}`}
-          value={parseFloat(reviews.average_rating.toFixed(1))}
-          precision={0.5}
-          readOnly
-          sx={{
-            "& .MuiRating-iconFilled": {
-              color: "#555555",
-            },
-            "& .MuiRating-iconHover": {
-              color: "pink",
-            },
-            "& .MuiRating-icon SVG": {
-              color: "#555",
-              fontSize: "0.8rem",
-            },
-          }}
-        />
-        <NavLink to={"/shop/:id"}>Link</NavLink>
-        <ShowMoreBtn
+          <Rating
+            name={`rating-${id}`}
+            value={parseFloat(reviews.average_rating.toFixed(1))}
+            precision={0.5}
+            readOnly
+            sx={{
+              "& .MuiRating-iconFilled": {
+                color: "#555555",
+              },
+              "& .MuiRating-iconHover": {
+                color: "pink",
+              },
+              "& .MuiRating-icon SVG": {
+                color: "#555",
+                fontSize: "0.8rem",
+              },
+            }}
+          />
+        </Review> */}
+            View details
+            {/* <ShowMoreBtn
           onClick={() => setShowFullDescription((prevState) => !prevState)}
-        ></ShowMoreBtn>
-        <Details>
+        ></ShowMoreBtn> */}
+            {/* <Details>
           {amount.map((size, i) => (
             <AmountBtn
               key={i}
@@ -62,11 +62,13 @@ export default function Product({ product, index }: Props) {
               <span key={i}>{size}</span>
             </AmountBtn>
           ))}
-        </Details>
-        <span>{price}EUR</span>
-        <Button>Buy</Button>
-      </ProductText>
-    </OuterBox>
+        </Details> */}
+            <span>{price}EUR</span>
+            <Button>Buy</Button>
+          </ProductText>
+        </OuterBox>
+      </StyledLink>
+    </>
   );
 }
 
@@ -76,8 +78,9 @@ const ProductText = styled.div`
   flex-direction: column;
   gap: 1rem;
   color: #505050;
+  font-size: var(--font-size-sm);
+
   @media (max-width: 1090px) {
-    font-size: 0.8rem;
     gap: 0.6rem;
   }
 
@@ -88,45 +91,36 @@ const ProductText = styled.div`
 
 const Review = styled.span`
   margin: 0;
-  font-size: 0.8rem;
+  font-size: var(--font-size-sm);
   display: flex;
   align-items: center;
   gap: 0.2rem;
-  @media (max-width: 900px) {
-    font-size: 0.7rem;
-  }
 `;
 
-const TotalReviews = styled.p`
-  font-size: 0.6rem;
-  @media (max-width: 900px) {
-    font-size: 0.55rem;
-  }
-`;
+// const TotalReviews = styled.p`
+//   font-size: 0.6rem;
+//   @media (max-width: 900px) {
+//     font-size: 0.55rem;
+//   }
+// `;
 
 const Title = styled.h3`
   margin: 0;
-  font-size: 1.15rem;
-  @media (max-width: 1090px) {
-    font-size: 1rem;
-  }
-  @media (max-width: 900px) {
-    font-size: 0.75rem;
-  }
+  font-size: var(--font-size-base);
 `;
 
-const ShowMoreBtn = styled.button`
-  width: fit-content;
-  font-size: 0.6rem;
-  background-color: transparent;
-  padding: 0;
-  cursor: pointer;
-  text-decoration: underline;
-`;
+// const ShowMoreBtn = styled.button`
+//   width: fit-content;
+//   font-size: var(--font-size-sm);
+//   background-color: transparent;
+//   padding: 0;
+//   cursor: pointer;
+//   text-decoration: underline;
+// `;
 
 const AmountBtn = styled.button<{ selected: boolean }>`
   width: fit-content;
-  font-size: 0.6rem;
+  font-size: var(--font-size-sm);
   background-color: transparent;
   padding: 0;
   border-radius: 0;
@@ -136,7 +130,7 @@ const AmountBtn = styled.button<{ selected: boolean }>`
 
 const Details = styled.div`
   margin: 0;
-  font-size: 0.781rem;
+  font-size: var(--font-size-sm);
   display: flex;
   gap: 1rem;
   color: #626262;
@@ -150,17 +144,31 @@ const Button = styled.button`
 `;
 
 const OuterBox = styled.div`
-  border-bottom: 1px solid var(--clr-dark-grey);
+  /* border-bottom: 1px solid var(--clr-dark-grey);
   border-right: 1px solid var(--clr-dark-grey);
-  padding: 1rem;
+  padding: 2rem 2rem 2rem 1rem;
+  gap: 1rem;
+  height: 100%;
+  align-items: center; */
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
+
+const StyledLink = styled(Link)`
+  border-bottom: 1px solid var(--clr-dark-grey);
+  border-right: 1px solid var(--clr-dark-grey);
+  padding: 2rem 2rem 2rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StyledImage = styled.img`
   object-fit: contain;
   position: relative;
   max-width: 160px;
+  min-width: 200px;
 
   @media (max-width: 900px) {
     width: 120px;
--- src/assets/pages/ShopPage.tsx
+++ src/assets/pages/ShopPage.tsx
@@ -1,31 +1,28 @@
-import { Container } from "@mui/material";
 import styled from "styled-components";
 import { products } from "../../data";
 import Product from "../components/Product";
 
 export default function ShopPage() {
   return (
-    <Container>
-      <InnerContainer>
-        <ShopTitle>Shop</ShopTitle>
-        <AllCategories>Filter by category</AllCategories>
-        <ProductGrid>
-          {products.map((product, index) => (
-            <Product key={index} product={product} index={index} />
-          ))}
-        </ProductGrid>
-      </InnerContainer>
-    </Container>
+    <InnerContainer>
+      <ShopTitle>Shop</ShopTitle>
+      <AllCategories>Filter by category</AllCategories>
+      <ProductGrid>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductGrid>
+    </InnerContainer>
   );
 }
 
 const InnerContainer = styled.div`
-  margin-top: 6rem;
+  margin: 6rem 0 3rem;
 `;
 
 const ProductGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
   margin-top: 2rem;
   border-top: 1px solid var(--clr-dark-grey);
   border-left: 1px solid var(--clr-dark-grey);
@@ -40,14 +37,14 @@ const ShopTitle = styled.h1`
 const AllCategories = styled.span`
   font-size: 1.2rem;
   position: relative;
-  color: #333;
+  color: var(--clr-dark-grey);
 
   &::after {
     position: absolute;
     content: "";
     left: 0;
     bottom: -0.1rem;
-    background-color: #333;
+    background-color: var(--clr-dark-grey);
     width: 100%;
     height: 1px;
   }
